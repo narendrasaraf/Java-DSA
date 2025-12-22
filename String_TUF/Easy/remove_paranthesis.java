009ad269ae11b7988eea3b6ee20937db6a23f11e
Narendra Saraf <Narendrasaraf91@outlook.com>
2025-12-22 13:18:39 +0530

    3Q 22/12 done string started

---
+++ String_TUF/Easy/remove_paranthesis.java
@@ -0,0 +1,28 @@
+package String_TUF.Easy;
+
+public class remove_paranthesis {
+    public static String removeparanthesis(String str) {
+        StringBuilder sb=new StringBuilder();
+        int depth=0;
+        for(char ch: str.toCharArray()) {
+            if (ch=='(') {
+                if (depth>0) {
+                    sb.append(ch);
+                }
+                depth++;
+            } 
+                else {
+                depth--;
+                if (depth>0) {
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str="(()())(())(()(()))";
+        String result=removeparanthesis(str);
+        System.out.println(result);
+    }
+}
