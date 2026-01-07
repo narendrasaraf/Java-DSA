e016b1fb52a4162737fd23bba299579992af9bfe
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-01-07 19:18:11 +0530

    small update

--- Binary_Search_TUF/1D/upper_bound.java
+++ Binary_Search_TUF/1D/upper_bound.java
@@ -1,21 +1,27 @@
 // package Binary_Search_TUF.1D;
 
 public class upper_bound {
-    public static int upperBound(int[] arr, int target) {
-    int low = 0, high = arr.length;
+    public static int optimal(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length-1;
+    int ans=nums.length;
 
-    while (low < high) {
-        int mid = low + (high - low) / 2;
+    while (low <= high) {
+        int mid =(high+low)/2;
 
-        if (arr[mid] <= target) {
+        if (nums[mid]>target) {
+            ans=mid;
             low = mid + 1;
         } else {
-            high = mid;
+            low=mid+1;
         }
     }
-    return low;
+        return ans; 
     }
     public static void main(String[] args) {
-        
+        int[] nums={-1,0,3,5,9,12};
+        int target=10;
+        int result1=optimal(nums,target);
+        System.out.println(result1);
     }
 }
