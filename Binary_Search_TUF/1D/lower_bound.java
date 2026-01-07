c1aabe0ddef5b633409cb489d5c876c2f33740c9
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-01-07 19:13:03 +0530

    small update

--- Binary_Search_TUF/1D/lower_bound.java
+++ Binary_Search_TUF/1D/lower_bound.java
@@ -2,18 +2,21 @@
 
 public class lower_bound {
     public static int optimal(int[] nums, int target) {
-    int low = 0, high = nums.length-1;
+    int low = 0;
+    int high = nums.length-1;
+    int ans=nums.length;
 
     while (low <= high) {
         int mid =(high+low)/2;
 
-        if (target>nums[mid]) {
+        if (nums[mid]>=target) {
+            ans=mid;
             low = mid + 1;
         } else {
-            high = mid;
+            low=mid+1;
         }
     }
-        return low; 
+        return ans; 
     }
     public static void main(String[] args) {
         int[] nums={-1,0,3,5,9,12};
