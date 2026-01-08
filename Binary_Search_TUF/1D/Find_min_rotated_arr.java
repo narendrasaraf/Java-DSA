cc86bef135eb0d48b91421484ea226f2a87d7c0e
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-01-08 19:00:42 +0530

    daily checkin 08/01

--- Binary_Search_TUF/1D/Find_min_rotated_arr.java
+++ Binary_Search_TUF/1D/Find_min_rotated_arr.java
@@ -3,9 +3,16 @@ public class Find_min_rotated_arr {
         int n=nums.length;
         int low=0;
         int high=n-1;
-        while(low<=high) {
-            
+        while(low<high) {
+            int mid=(low+high)/2;
+            if(nums[mid]<=nums[high]) high=mid;
+            else low=mid+1;
         }
-        return -1;
+        return nums[low];
+    }
+    public static void main(String[] args) {
+        int[] nums={3,4,5,1,2};
+        int result=optimal(nums);
+        System.out.println(result);
     }
 }
