84425968d813859c8dcff66ab2ed15c433e6f50d
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-01-09 18:32:42 +0530

    update

--- Binary_Search_TUF/1D/Single_element.java
+++ Binary_Search_TUF/1D/Single_element.java
@@ -1,13 +1,24 @@
 public class Single_element {
     public static int optimal(int[] nums) {
         int n=nums.length;
-        int low=0;
-        int high=n-1;
+        int low=1;
+        int high=n-2;
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
         while(low<=high) {
+            int mid=(low+high)/2;
 
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+
+            if(mid%2==1 && nums[mid]==nums[mid-1] || mid %2==0 && nums[mid]==nums[mid+1]) low=mid+1;
+            else high=mid-1; 
         }
-        int ans=0;
-        ans=nums^nums;
-        return ;
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums={1,1,2,3,3,4,4,8,8};
+        int result=optimal(nums);
+        System.out.println(result);
     }
 }
