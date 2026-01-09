78f4c006c4bad0b63ca97419fa2b1fcdde0c955e
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-01-09 21:37:51 +0530

    update

---
+++ Binary_Search_TUF/1D/find_peak_element.java
@@ -0,0 +1,13 @@
+public class find_peak_element {
+    public static int optimal(int[] nums) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high) {
+            int mid=(low+high)/2;
+
+        }
+        
+        return -1;
+    }
+}
