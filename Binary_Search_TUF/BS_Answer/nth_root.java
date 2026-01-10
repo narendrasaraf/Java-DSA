7f75d05dd25ebd0e4005efd9e9475f2a2be3cc76
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-01-10 15:13:35 +0530

    10/10

--- Binary_Search_TUF/BS_Answer/nth_root.java
+++ Binary_Search_TUF/BS_Answer/nth_root.java
@@ -1,5 +1,26 @@
 package Binary_Search_TUF.BS_Answer;
 
 public class nth_root {
-    
+    public static int optimal(int n,int number) {
+        int low=1;
+        int high=number;
+        int ans=0;
+        if(n==0 || n==1) return number;
+        while (low<=high) {
+            int mid=(low+high)/2;
+            long result = 1; 
+            for(int i=0;i<n;i++) {
+            result=result*mid;
+            }
+            if (result == number) return mid;
+            else if (result < number) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int n=3;
+        int number=81;
+        System.out.println(optimal(n,number));
+    }
 }
