f122d843c701e29de3b3bdf1f6a805081e50f23d
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-01-10 22:12:40 +0530

    update

---
+++ Binary_Search_TUF/BS_Answer/koko_banana.java
@@ -0,0 +1,41 @@
+package Binary_Search_TUF.BS_Answer;
+
+public class koko_banana {
+    public static int minEatingSpeed(int[] piles,int h) {
+        int low=1;
+        int high=maxPile(piles);
+        int ans=high;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            int hoursNeeded=calculateHours(piles,mid);
+            if(hoursNeeded<=h) {
+                ans=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static long calculateHours(int[] piles,int hourly) {
+        long hours=0;
+        for(int pile:piles) {
+            hours=hours+(int)Math.ceil((double)pile/hourly);
+        }
+        return hours;
+    }
+    public static int maxPile(int[] piles) {
+        int max=Integer.MIN_VALUE;
+        for(int pile:piles) {
+            if(pile>max) {
+                max=pile;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] piles={30,11,23,4,20};
+        int h=6;
+        System.out.println(minEatingSpeed(piles,h));
+    }
+}
