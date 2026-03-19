c2fafeee3b9ed7d2dcfd38ce1a9e7b3869d28133
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-03-19 22:11:40 +0530

    update

---
+++ Arrays/hard.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class hard {
+    
+
+    List<Long> list;
+    long mul, add;
+
+    public Fancy() {
+        list = new ArrayList<>();
+        mul = 1;
+        add = 0;
+    }
+    
+    public void append(int val) {
+        long normalized = (val - add + MOD) % MOD;
+        normalized = (normalized * modInverse(mul)) % MOD;
+        list.add(normalized);
+    }
+    
+    public void addAll(int inc) {
+        add = (add + inc) % MOD;
+    }
+    
+    public void multAll(int m) {
+        mul = (mul * m) % MOD;
+        add = (add * m) % MOD;
+    }
+    
+    public int getIndex(int idx) {
+        if (idx >= list.size()) return -1;
+
+        long val = list.get(idx);
+        return (int)((val * mul + add) % MOD);
+    }
+
+    // Fermat’s Little Theorem
+    private long modInverse(long x) {
+        return power(x, MOD - 2);
+    }
+
+    private long power(long x, long y) {
+        long res = 1;
+        x %= MOD;
+
+        while (y > 0) {
+            if ((y & 1) == 1) res = (res * x) % MOD;
+            x = (x * x) % MOD;
+            y >>= 1;
+        }
+        return res;
+    }
+}
