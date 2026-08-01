d212274b85f0b0be45a2efc5643a167cd818349d
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-08-01 14:31:41 +0530

    dp started

---
+++ DP/fib.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class fib{
+    int fib(int n) {
+        // Tabulation
+
+        // int dp[]=new int[n+1];
+        // if(n<=1) return n;
+        // dp[0]=0;
+        // dp[1]=1;
+        // for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
+        // return dp[n];
+
+        // memoization
+        int dp[]=new int[n+1];
+        if(n<=1) return n;
+        Arrays.fill(dp,-1);
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=fib(n-1)+fib(n-2);
+        return dp[n];
+    }
+
+}
